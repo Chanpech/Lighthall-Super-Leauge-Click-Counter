a2be26837a8a72fbc5f8b0a08bc0644e363ee349
Chanpech <70418825+Chanpech@users.noreply.github.com>
2023-04-14 16:56:40 -0400

    Migrate to project to GitHub

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import React, {useState, useEffect} from 'react'; 
+
+function App() {
+    const [count, setCount] = useState(
+        Number(window.localStorage.getItem('count') || 0)
+    );
+    useEffect(() => {
+        if(window.localStorage.getItem("count") !== undefined){
+            window.localStorage.setItem('count', count);
+        }else{
+            window.localStorage.setItem('count', 0);
+        }
+    }, [count]);
+  return (
+    <div className="App-header">
+      <header>
+        Click-Counter
+      </header>
+      <div className='button_container'>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}> Increment Count</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
